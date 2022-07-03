541560789518079b4dc198e8301f70f34088c54c
wajeehamalik913 <wajeehamalik913@gmail.com>
2022-07-03 23:39:14 +0500

    complete application

--- app/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import { user } from './user'
-
-// We'll be combining reducers ahead, when we implement other functionalities like having friends 
-const Reducers = combineReducers({
-    userState: user,
-})
-
-export default Reducers
